0cbcc84daedf323de78ac47e564285d1d83944bd
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-02-25 14:40:12 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -1,16 +1,25 @@
 package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping("/")
+//	@RequestMapping(value = "/login" , method =  RequestMethod.GET)
+	@GetMapping("/")
 	public String greeting()
 	{
 		System.out.println("WelcomeController.greeting()");
 		return "index";
+	}
+	
+	
+	@GetMapping("/sign-up")
+	public String createUser()
+	{
+		System.out.println("WelcomeController.greeting()");
+		return "success";
 	}
 
 }
