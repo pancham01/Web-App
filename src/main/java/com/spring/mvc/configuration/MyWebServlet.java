be96d268ab522a17ba3a65e3f6e007dbe92f75a0
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-03-23 15:40:01 +0530

    update

--- src/main/java/com/spring/mvc/configuration/MyWebServlet.java
+++ src/main/java/com/spring/mvc/configuration/MyWebServlet.java
@@ -1,12 +1,12 @@
 package com.spring.mvc.configuration;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 
 public class MyWebServlet implements WebApplicationInitializer {
 	@Override
